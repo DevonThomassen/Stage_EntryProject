c3bb715d8f8ab6db72c7ced30812bb94ce772310
DevonThomassen <devon.thomassen@hotmail.com>
2020-02-11 14:57:23 +0100

    Base interceptor

---
+++ src/app/interceptors/base.interceptor.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpResponse
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class BaseInterceptor implements HttpInterceptor {
+
+  constructor() { }
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+
+    const req = request.clone({
+      url: `https://testapi.jarpiscloud.nl/api/v1/${request.url}`
+    });
+
+
+    return next.handle(req);
+  }
+}
