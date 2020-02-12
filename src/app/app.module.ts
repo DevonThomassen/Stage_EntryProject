93524a56818b720c626166a6fcbb82d172b54026
DevonThomassen <devon.thomassen@hotmail.com>
2020-02-12 15:45:10 +0100

    jwt zooi :(

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { JwtModule } from "@auth0/angular-jwt";
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,7 +8,7 @@ import { AppComponent } from './app.component';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BaseInterceptor } from './interceptors/base.interceptor';
 import { ErrorHandelerInterceptor } from './interceptors/error-handeler.interceptor';
-// import { JwtInterceptor } from './interceptors/jwt.interceptor';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [
@@ -16,12 +17,18 @@ import { ErrorHandelerInterceptor } from './interceptors/error-handeler.intercep
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: () => { return localStorage.getItem('token') },
+        whitelistedDomains: ['testapi.jarpiscloud.nl']
+      }
+    })
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: BaseInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: ErrorHandelerInterceptor, multi: true },
-    // { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
