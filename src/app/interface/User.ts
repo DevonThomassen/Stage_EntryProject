5fd15d4daa3424a03993e749856082847825a52d
DevonThomassen <devon.thomassen@hotmail.com>
2020-02-12 15:42:37 +0100

    interface user

---
+++ src/app/interface/User.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id: number;
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  avatar: string;
+  created_at: string;
+  updated_at: string;
+}
