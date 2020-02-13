6003eba1c79bebb88459f89e082d190d2210420a
DevonThomassen <devon.thomassen@hotmail.com>
2020-02-13 10:58:14 +0100

    dashboard/app-bar components loads the currentuser

---
+++ src/app/dashboard/app-bar/app-bar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../authentication/authentication.service';
+import { User } from 'src/app/interface/User';
+
+
+@Component({
+  selector: 'app-app-bar',
+  templateUrl: './app-bar.component.html',
+  styleUrls: ['./app-bar.component.css']
+})
+export class AppBarComponent implements OnInit {
+
+  user: User;
+  constructor(private auth: AuthenticationService) { }
+
+  ngOnInit(): void {
+    this.auth.currentUser.subscribe(user => { this.user = user; });
+    console.log(this.user);
+  }
+
+}
