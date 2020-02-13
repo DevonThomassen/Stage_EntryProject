629d852c4f1462fe82bb51d0d5316a3f3e47de14
DevonThomassen <devon.thomassen@hotmail.com>
2020-02-13 13:52:17 +0100

    dashboard/user-detail component init

---
+++ src/app/dashboard/user-detail/user-detail.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-user-detail',
+  templateUrl: './user-detail.component.html',
+  styleUrls: ['./user-detail.component.scss']
+})
+export class UserDetailComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    console.log(`DE ID is: ${id}`);
+  }
+
+}
