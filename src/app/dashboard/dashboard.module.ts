5d7d54b7f1835494be6d91003f93ad910ebb13e7
DevonThomassen <devon.thomassen@hotmail.com>
2020-02-13 10:58:47 +0100

    Add new components to to dashboard module

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { AppBarComponent } from './app-bar/app-bar.component';
+import { UsersComponent } from './users/users.component';
 
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, AppBarComponent, UsersComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule
