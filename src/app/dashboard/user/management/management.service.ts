13cdd9a436d6400d77aee4e56e4cb983234e9dfb
DevonThomassen <devon.thomassen@hotmail.com>
2020-02-17 12:02:05 +0100

    Add user in management service

---
+++ src/app/dashboard/user/management/management.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AddUser } from 'src/app/interface/AddUser';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ManagementService {
+
+  constructor(private http: HttpClient) { }
+
+  AddUser(user: AddUser) {
+    return this.http.post('/users', user).pipe();
+  }
+}
