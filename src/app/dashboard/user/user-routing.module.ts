5ad0a0763a98ec9df76ad1eb05dec2aa4172a6f5
DevonThomassen <devon.thomassen@hotmail.com>
2020-02-17 14:21:47 +0100

    Routing path

--- src/app/dashboard/user/user-routing.module.ts
+++ src/app/dashboard/user/user-routing.module.ts
@@ -3,13 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user.component';
 import { DetailComponent } from './detail/detail.component';
 import { AuthGuard } from 'src/app/auth/auth.guard';
-import { ManagementComponent } from './management/management.component';
+import { AddComponent } from './add/add.component';
 
 
 const routes: Routes = [
   { path: '', component: UserComponent, canActivate: [AuthGuard] },
   { path: 'user/detail/:id', component: DetailComponent, canActivate: [AuthGuard]},
-  { path: 'user/management/add', component: ManagementComponent, canActivate: [AuthGuard]}
+  { path: 'user/add', component: AddComponent, canActivate: [AuthGuard]},
 ];
 
 @NgModule({
