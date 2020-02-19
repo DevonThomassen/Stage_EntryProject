0b3a69e1371e677d85a6b00076cf850720af9eaf
DevonThomassen <devon.thomassen@hotmail.com>
2020-02-19 09:17:39 +0100

    token and currentUser type changes

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { BehaviorSubject, ReplaySubject, Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { Router } from '@angular/router';
 
 import { User } from '../interface/User';
 
@@ -11,17 +12,8 @@ import { User } from '../interface/User';
 })
 export class AuthService {
 
-  private token: string;
-  private currentUser: BehaviorSubject<User> = new BehaviorSubject({
-    id: 0,
-    email: '',
-    password: '',
-    firstName: 'Start',
-    lastName: 'Name',
-    avatar: 'https://robohash.org/consecteturmagniid.jpg?size=100x100&set=set1',
-    created_at: '',
-    updated_at: ''
-  });
+  private token: BehaviorSubject<string> = new BehaviorSubject('');
+  private currentUser: ReplaySubject<User> = new ReplaySubject(1);
 
   constructor(
     private http: HttpClient, private helper: JwtHelperService, private router: Router) {
@@ -40,19 +32,24 @@ export class AuthService {
   }
 
   isAuthenticated(): boolean {
+    return !this.helper.isTokenExpired(this.getToken());
   }
 
   logIn(credentials: { email: string, password: string }): Observable<{ token: string }> {
     return this.http.post('/auth/login', credentials).pipe(
       tap((data: { token: string }) => {
-        this.token = data.token;
-        this.currentUser.next(this.helper.decodeToken(this.token));
+        this.token.next(data.token);
+        localStorage.setItem('Token', this.token.value);
+        this.currentUser.next(this.helper.decodeToken(this.token.value));
         console.log([this.token, this.currentUser]);
       })
     );
   }
 
   loggedIn(): boolean {
+    return !!this.token.value;
+  }
+
   logOut() {
     this.token.next('');
     this.currentUser.next();
