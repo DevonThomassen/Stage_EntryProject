d38b6773c8bc3decf5eb567e451ec242f9b8b85c
DevonThomassen <devon.thomassen@hotmail.com>
2020-03-02 14:34:38 +0100

    Sharemodule

--- src/app/shared-module/shared-module.module.ts
+++ src/app/shared-module/shared-module.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DialogComponent } from '../dialog/dialog.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [DialogComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
-  exports: [DialogComponent]
+  exports: [DialogComponent, ReactiveFormsModule]
 })
 export class SharedModuleModule { }
